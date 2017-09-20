5e5e296f590e5d524d415bc5356d019b4979af60
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-09-20 13:59:23 -0400

    Updated from Brython Server: 9/20/2017 1:59:21 PM

--- house.py
+++ house.py
@@ -1,1 +1,12 @@
+#Greg Phillips
+#9/20/17
+#house.py - displays a picture of a house
 
+from ggame import * 
+
+white = Color()xffffff,1)
+blackOutline = LineStyle(1,black) #(pixels thick,color)
+
+whiteRectangle = RectangleAsset(200,100,blackOutline,red) #(width,height,outline,fill)
+
+App().run()
