d4be1410d8877d46d255746641883943c7c23dae
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-09-22 14:45:53 -0400

    Updated from Brython Server: 9/22/2017 2:45:52 PM

--- germanFlag.py
+++ germanFlag.py
@@ -1,1 +1,21 @@
+#Greg Phillips
+#9/20/17
+#germanFlag.py - german flag
 
+from ggame import * 
+
+red = Color(0xff0000,1)
+black = Color(0x000000,1)
+yellow = Color(0xffff00,1)
+
+blackOutline = LineStyle(1,black) #(pixels thick,color)
+
+blackRectangle = RectangleAsset(200,30,blackOutline,black)
+redRectangle = RectangleAsset(200,30,blackOutline,red)
+yellowRectangle = RectangleAsset(200,30,blackOutline,yellow)
+
+Sprite(blackRectangle)
+Sprite(redRectangle, (0,30))
+Sprite(yellowRectangle, (0,60))
+
+App().run()
