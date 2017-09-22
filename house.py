a22348cbdf348997d1453c499852034f81363dcf
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-09-22 14:37:06 -0400

    Updated from Brython Server: 9/22/2017 2:37:00 PM

--- house.py
+++ house.py
@@ -9,7 +9,7 @@ white = Color(0xffffff,1)
 blackOutline = LineStyle(1,black) #(pixels thick,color)
 
 whiteRectangle = RectangleAsset(200,100,blackOutline,white) #(width,height,outline,fill)
-whiteTriangle = PolygonAsset([(100,0), (0,100), (200,100)], blackOutline, white)
+whiteTriangle = PolygonAsset([(100,0), (0,100), (200,100)], blackOutline, black)
 whiteDoor = RectangleAsset(30,70,blackOutline,white)
 
 Sprite(whiteTriangle)
